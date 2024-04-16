154e12f3f1f07a3a08a731df0356fdecc8c50b2a
TimaliveProgman <timur20051610@gmail.com>
2024-04-16 22:09:39 +0500

    create StateProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final counterProvider = StateProvider((ref) => 0);
+
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
